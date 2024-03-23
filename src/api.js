6bd43569a5abb47e6b0fe92b709d7af74a4d0f02
michael <michaelhernandezrala@gmail.com>
2024-03-23 10:42:30 +0100

    chore: change helmet config

--- src/api.js
+++ src/api.js
@@ -11,16 +11,7 @@ import logger from './helpers/logger.js';
 const app = express();
 
 app.use(cors());
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-        'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://unpkg.com'],
-      },
-    },
-  })
-);
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
