871cd9d5e79a8237cb73645191b083f2d591bed3
michael <michaelhernandezrala@gmail.com>
2024-03-23 10:33:21 +0100

    docs: add swagger

--- src/api.js
+++ src/api.js
@@ -11,7 +11,16 @@ import logger from './helpers/logger.js';
 const app = express();
 
 app.use(cors());
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://unpkg.com'],
+      },
+    },
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
