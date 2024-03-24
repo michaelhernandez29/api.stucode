e927dacb02a8636747bbfe5df7905331afe8beb2
michael <michaelhernandezrala@gmail.com>
2024-03-24 19:48:35 +0100

    fix: fix comment in PUT /user/{id}

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,6 +24,28 @@ const findById = async (req, res) => {
   responseHelper.ok(res, user);
 };
 
+/**
+ * Handler for PUT /user/{id}
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ */
+const updateById = async (req, res) => {
+  const id = req.params.id;
+  const payload = req.body;
+
+  let user = await userService.findById(id);
+  if (!user) {
+    responseHelper.notFound(res, errorMessages.USER_NOT_FOUND);
+    return;
+  }
+
+  let response = await userService.updateById(id, payload);
+  response = _.omit(response, 'password');
+
+  responseHelper.ok(res, response);
+};
+
 userController.findById = findById;
+userController.updateById = updateById;
 
 module.exports = userController;
--- src/services/userService.js
+++ src/services/userService.js
@@ -20,7 +20,19 @@ const findByEmail = async (email) => {
   return user.findOne({ where: { email }, raw: true });
 };
 
+/**
+ * Updates a user by ID.
+ * @param {String} id - The ID of the user to update.
+ * @param {Object} data - The data to update for the user.
+ * @returns {Promise<Object|null>} A promise that resolves to the updated user object or null if not found.
+ */
+const updateById = async (id, data) => {
+  const updateUser = await user.update(data, { where: { id }, returning: true, raw: true });
+  return updateUser[1][0];
+};
+
 userService.findById = findById;
 userService.findByEmail = findByEmail;
+userService.updateById = updateById;
 
 module.exports = userService;
