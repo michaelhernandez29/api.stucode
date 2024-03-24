87795d8d223c13486902dc07b0da05b4b6962ede
michael <michaelhernandezrala@gmail.com>
2024-03-24 12:10:26 +0100

    fix: some texts fixes in the error messages

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -4,8 +4,8 @@ module.exports = {
   PASSWORD_NOT_VALID: 'The password is not valid',
 
   // 404 NOT_FOUND
-  ACCOUNT_NOT_FOUND: 'ACCOUNT_NOT_FOUND',
-  USER_NOT_FOUND: 'USER_NOT_FOUND',
+  ACCOUNT_NOT_FOUND: 'The account does not exist',
+  USER_NOT_FOUND: 'The user does not exist',
 
   // 409 CONFLICT
   EMAIL_ALREADY_EXISTS: 'The email already exists',
