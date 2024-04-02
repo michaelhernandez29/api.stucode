e3e27dcd47358cd8a01f7092d9ec98dd7abc969f
michael <michaelhernandezrala@gmail.com>
2024-04-02 18:45:00 +0200

    test: first test

--- __tests__/controllers/authController.spec.js
+++ __tests__/controllers/authController.spec.js
@@ -1,34 +1,40 @@
-// const request = require('supertest');
+const request = require('supertest');
 
-// const app = require('../../src/api');
+const app = require('../../src/api');
 
 describe('authController', () => {
   describe('/v1/auth/login', () => {
     it('returns 200 when signin request is valid', async () => {
-      // let data = {
-      //   email: 'test@test.com',
-      //   password: 'test',
-      // };
-      // const response = await request(app).post('/v1/auth/login').send(data);
-      // expect(response.status).toBe(200);
-    });
+      let data = {
+        email: 'test@test.com',
+        password: 'test',
+      };
 
-    it('returns Ok when signin request is valid', async () => {
-      // let data = {
-      //   email: 'test@test.com',
-      //   password: 'test',
-      // };
-      // const response = await request(app).post('/v1/auth/login').send(data);
-      // expect(response.body.message).toBe('OK');
-    });
+      const response = await request(app).post('/v1/auth/login').send(data);
 
-    it('returns data when signin request is valid', async () => {
-      // let data = {
-      //   email: 'test@test.com',
-      //   password: 'test',
-      // };
-      // const response = await request(app).post('/v1/auth/login').send(data);
-      // expect(response.body.data).not.toBeNull();
-    });
+      expect(response.status).toBe(200);
+    }, 20000);
+
+    // it('returns Ok when signin request is valid', async () => {
+    //   let data = {
+    //     email: 'test@test.com',
+    //     password: 'test',
+    //   };
+
+    //   const response = await request(app).post('/v1/auth/login').send(data);
+
+    //   expect(response.body.message).toBe('OK');
+    // });
+
+    // it('returns data when signin request is valid', async () => {
+    //   let data = {
+    //     email: 'test@test.com',
+    //     password: 'test',
+    //   };
+
+    //   const response = await request(app).post('/v1/auth/login').send(data);
+
+    //   expect(response.body.data).not.toBeNull();
+    // });
   });
 });
