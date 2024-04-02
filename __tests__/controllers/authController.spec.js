8b3e52e924b4bb74eaf67b8c8fac154d5b77542d
michael <michaelhernandezrala@gmail.com>
2024-04-02 18:41:04 +0200

    test: some test

--- __tests__/controllers/authController.spec.js
+++ __tests__/controllers/authController.spec.js
@@ -1,52 +1,34 @@
-const request = require('supertest');
+// const request = require('supertest');
 
-const app = require('../../src/api');
+// const app = require('../../src/api');
 
 describe('authController', () => {
   describe('/v1/auth/login', () => {
-    it('returns 200 when signin request is valid', (done) => {
-      let data = {
-        email: 'test@test.com',
-        password: 'test',
-      };
-
-      request(app)
-        .post('/v1/auth/login')
-        .send(data)
-        .then((response) => {
-          expect(response.status).toBe(200);
-          done();
-        });
+    it('returns 200 when signin request is valid', async () => {
+      // let data = {
+      //   email: 'test@test.com',
+      //   password: 'test',
+      // };
+      // const response = await request(app).post('/v1/auth/login').send(data);
+      // expect(response.status).toBe(200);
     });
 
-    it('returns Ok when signin request is valid', (done) => {
-      let data = {
-        email: 'test@test.com',
-        password: 'test',
-      };
-
-      request(app)
-        .post('/v1/auth/login')
-        .send(data)
-        .then((response) => {
-          expect(response.body.message).toBe('OK');
-          done();
-        });
+    it('returns Ok when signin request is valid', async () => {
+      // let data = {
+      //   email: 'test@test.com',
+      //   password: 'test',
+      // };
+      // const response = await request(app).post('/v1/auth/login').send(data);
+      // expect(response.body.message).toBe('OK');
     });
 
-    it('returns data when signin request is valid', (done) => {
-      let data = {
-        email: 'test@test.com',
-        password: 'test',
-      };
-
-      request(app)
-        .post('/v1/auth/login')
-        .send(data)
-        .then((response) => {
-          expect(response.body.data).not.toBeNull();
-          done();
-        });
+    it('returns data when signin request is valid', async () => {
+      // let data = {
+      //   email: 'test@test.com',
+      //   password: 'test',
+      // };
+      // const response = await request(app).post('/v1/auth/login').send(data);
+      // expect(response.body.data).not.toBeNull();
     });
   });
 });
