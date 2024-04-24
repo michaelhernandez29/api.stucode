4512231a2b1bcf6a24a7ebdba013965a13c76371
michael <michaelhernandezrala@gmail.com>
2024-04-24 20:24:58 +0200

    test: changes in the /auth/login test

--- tests/controllers/authController.test.js
+++ tests/controllers/authController.test.js
@@ -20,24 +20,7 @@ describe('authController', () => {
       };
       const response = await request(app).post('/v1/auth/login').send(data);
       expect(response.status).toBe(200);
-    });
-
-    it('returns OK when signin request is valid', async () => {
-      let data = {
-        email: 'test@test.com',
-        password: 'test',
-      };
-
-      const response = await request(app).post('/v1/auth/login').send(data);
       expect(response.body.message).toBe('OK');
-    });
-
-    it('returns data when signin request is valid', async () => {
-      let data = {
-        email: 'test@test.com',
-        password: 'test',
-      };
-      const response = await request(app).post('/v1/auth/login').send(data);
       expect(response.body.data).not.toBeNull();
     });
   });
