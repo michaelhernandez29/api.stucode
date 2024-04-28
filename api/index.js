03bd2c6b2623f43888465268273523f3e0768392
michael <michaelhernandezrala@gmail.com>
2024-04-28 17:20:08 +0200

    refactor: some refactor in the server setup

--- api/index.js
+++ api/index.js
@@ -1,9 +1,45 @@
-const app = require('./api');
+require('dotenv').config();
+const app = require('express')();
+
+const fs = require('fs');
+const cors = require('cors');
+const yml = require('js-yaml');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const OpenApiValidator = require('express-openapi-validator');
+
 const config = require('./config');
+const authHandler = require('./middlewares/authHandler');
+const errorHandler = require('./middlewares/errorHandler');
 const logger = require('./helpers/logger');
 
 const PORT = config.get('port');
 
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const swaggerDocument = yml.load(fs.readFileSync(__dirname + '/openapi/api.yml', 'utf-8'));
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: __dirname + '/openapi/api.yml',
+    operationHandlers: __dirname + '/controllers',
+    validateSecurity: {
+      handlers: {
+        bearerAuth: authHandler,
+      },
+    },
+  }),
+);
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   logger.info({ message: `Server listening on port ${PORT}` });
 });
+
+module.exports = app;
