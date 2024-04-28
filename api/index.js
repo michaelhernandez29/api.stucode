0a1b94cc76cdbe2404968baf48a54add851c7ebd
michael <michaelhernandezrala@gmail.com>
2024-04-28 18:43:32 +0200

    fix: uncomment code

--- api/index.js
+++ api/index.js
@@ -31,7 +31,6 @@ app.use(
     customCssUrl: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
     ],
   }),
 );
