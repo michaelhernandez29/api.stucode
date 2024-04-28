ff662f9e5423f6b2a6a4525169b3af1b0e686b7d
michael <michaelhernandezrala@gmail.com>
2024-04-28 17:52:15 +0200

    test: some uncomment code for test vercel

--- api/index.js
+++ api/index.js
@@ -24,7 +24,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const swaggerDocument = yml.load(fs.readFileSync(__dirname + '/openapi/api.yml', 'utf-8'));
 app.get('/', (req, res) => res.send('Express on Vercel'));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    customCssUrl: 'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css',
+  }),
+);
 // app.use(
 //   OpenApiValidator.middleware({
 //     apiSpec: __dirname + '/openapi/api.yml',
