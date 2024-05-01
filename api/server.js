2d2520ea3728ab47451a9b7115641a5ae00eb37b
michael <michaelhernandezrala@gmail.com>
2024-05-01 18:53:33 +0200

    chore: setup test config

---
+++ api/server.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const app = require('express')();
+
+const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const Redoc = require('redoc-express');
+const OpenApiValidator = require('express-openapi-validator');
+
+const authHandler = require('./middlewares/authHandler');
+const errorHandler = require('./middlewares/errorHandler');
+
+app.use(cors());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'", 'unpkg.com'],
+      workerSrc: ["'self'", 'blob:'],
+    },
+  }),
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.send('Express on Vercel'));
+
+app.get('/api/api.yml', (req, res) => {
+  res.sendFile('openapi/api.yml', { root: path.resolve(__dirname) });
+});
+
+app.use(
+  '/api-docs',
+  Redoc({
+    title: 'StuCode API Docs',
+    specUrl: '/api/api.yml',
+  }),
+);
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: __dirname + '/openapi/api.yml',
+    operationHandlers: __dirname + '/controllers',
+    validateSecurity: {
+      handlers: {
+        bearerAuth: authHandler,
+      },
+    },
+  }),
+);
+
+app.use(errorHandler);
+
+module.exports = app;
