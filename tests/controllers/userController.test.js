b4e2c531d90505e5dea4ecd39cee19eec5efa1d8
michael <michaelhernandezrala@gmail.com>
2024-05-01 19:07:25 +0200

    test: add POST /user/login tests

--- tests/controllers/userController.test.js
+++ tests/controllers/userController.test.js
@@ -9,27 +9,28 @@ const errorMessages = require('../../api/constants/errorMessages');
 const prisma = new PrismaClient();
 
 let app = null;
+const newUserCompleteBody = {
+  name: 'test',
+  email: 'test@test.com',
+  password: 'test',
+};
+const userCompleteBody = { email: 'test@test.com', password: 'test' };
+
 beforeEach(async () => {
   app = server.listen(config.get('port'));
   await prisma.$connect();
 });
 
 describe('POST /user/register', () => {
-  const completeBody = {
-    name: 'test',
-    email: 'test@test.com',
-    password: 'test',
-  };
-
-  it("should respond with 200 OK if email doesn't exist", async () => {
-    const response = await request(app).post('/v1/user/register').send(completeBody);
+  it("should respond with 201 Created if email doesn't exist", async () => {
+    const response = await request(app).post('/v1/user/register').send(newUserCompleteBody);
 
     expect(response.status).toBe(201);
     expect(response.body.statusCode).toBe(201);
     expect(response.body.message).toEqual('Created');
     expect(response.body).toHaveProperty('data');
-    expect(response.body.data.name).toEqual(completeBody.name);
-    expect(response.body.data.email).toEqual(completeBody.email);
+    expect(response.body.data.name).toEqual(newUserCompleteBody.name);
+    expect(response.body.data.email).toEqual(newUserCompleteBody.email);
   });
 
   it('should respond with 400 Bad Request if email format is not valid', async () => {
@@ -47,13 +48,54 @@ describe('POST /user/register', () => {
   });
 
   it('should respond with 409 Conflict if email already exists', async () => {
-    await request(app).post('/v1/user/register').send(completeBody);
-    const response = await request(app).post('/v1/user/register').send(completeBody);
+    await request(app).post('/v1/user/register').send(newUserCompleteBody);
+    const response = await request(app).post('/v1/user/register').send(newUserCompleteBody);
 
     expect(response.status).toBe(409);
     expect(response.body.statusCode).toBe(409);
     expect(response.body.message).toEqual(errorMessages.EMAIL_ALREADY_EXISTS);
     expect(response.body.errorCode).toEqual(errorCodes.CONFLICT);
+  });
+});
+
+describe('POST /user/login', () => {
+  it('should respond with 200 OK if user has logged correctly', async () => {
+    await request(app).post('/v1/user/register').send(newUserCompleteBody);
+    const response = await request(app).post('/v1/user/login').send(userCompleteBody);
+
+    expect(response.status).toBe(200);
+    expect(response.body.statusCode).toBe(200);
+    expect(response.body.message).toEqual('OK');
+    expect(response.body).toHaveProperty('data');
+  });
+
+  it('should respond with 400 Bad Request if credentials are not valid', async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'test1',
+    };
+
+    await request(app).post('/v1/user/register').send(newUserCompleteBody);
+
+    const response = await request(app).post('/v1/user/login').send(data);
+    expect(response.status).toBe(400);
+    expect(response.body.statusCode).toBe(400);
+    expect(response.body.message).toEqual(errorMessages.PASSWORD_NOT_VALID);
+    expect(response.body.errorCode).toEqual(errorCodes.BAD_REQUEST);
+  });
+
+  it("should respond with 404 Not Found if email doesn't exist", async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'test1',
+    };
+
+    const response = await request(app).post('/v1/user/login').send(data);
+
+    expect(response.status).toBe(404);
+    expect(response.body.statusCode).toBe(404);
+    expect(response.body.message).toEqual(errorMessages.USER_NOT_FOUND);
+    expect(response.body.errorCode).toEqual(errorCodes.NOT_FOUND);
   });
 });
 
