be4970d378eff8eb1a00be4589e68ced58e1c3de
michael <michaelhernandezrala@gmail.com>
2024-05-01 18:54:19 +0200

    test: add POST /user/register tests

--- tests/controllers/userController.test.js
+++ tests/controllers/userController.test.js
@@ -1,145 +1,64 @@
 const request = require('supertest');
+const { PrismaClient } = require('@prisma/client');
 
-const app = require('../../src/api');
-const db = require('../../src/lib/db');
+const config = require('../../api/config');
+const server = require('../../api/server');
+const errorCodes = require('../../api/constants/errorCodes');
+const errorMessages = require('../../api/constants/errorMessages');
 
-describe('userController', () => {
-  let id = '7e260ebd-7a78-4584-bb97-d4bb650bc06d';
-  let login;
-  let data = {
+const prisma = new PrismaClient();
+
+let app = null;
+beforeEach(async () => {
+  app = server.listen(config.get('port'));
+  await prisma.$connect();
+});
+
+describe('POST /user/register', () => {
+  const completeBody = {
+    name: 'test',
     email: 'test@test.com',
     password: 'test',
   };
 
-  beforeAll(async () => {
-    login = await request(app).post('/v1/user/login').send(data);
-    await db.authenticate();
+  it("should respond with 200 OK if email doesn't exist", async () => {
+    const response = await request(app).post('/v1/user/register').send(completeBody);
+
+    expect(response.status).toBe(201);
+    expect(response.body.statusCode).toBe(201);
+    expect(response.body.message).toEqual('Created');
+    expect(response.body).toHaveProperty('data');
+    expect(response.body.data.name).toEqual(completeBody.name);
+    expect(response.body.data.email).toEqual(completeBody.email);
   });
 
-  afterAll(async () => {
-    await db.close();
-  });
-
-  describe('GET /v1/user', () => {
-    it('returns 200 when request is valid', async () => {
-      const response = await request(app)
-        .get(`/v1/user`)
-        .set({
-          Authorization: `Bearer ${login.body.data}`,
-        });
-
-      expect(response.status).toBe(200);
-      expect(response.body.statusCode).toBe(200);
-      expect(response.body.message).toEqual('OK');
-      expect(response.body).toHaveProperty('data');
-      expect(typeof response.body.data).toBe('object');
-      expect(response.body.data.length).toBeLessThanOrEqual(20);
-      expect(response.body).toHaveProperty('count');
-    });
-
-    it('returns 401 when request is unauthorized', async () => {
-      const response = await request(app).get(`/v1/user`).set({
-        Authorization: `Bearer test`,
-      });
-
-      expect(response.status).toBe(401);
-      expect(response.body.statusCode).toBe(401);
-    });
-  });
-
-  describe('GET /v1/user/{id}', () => {
-    it('returns 200 when request is valid', async () => {
-      const response = await request(app)
-        .get(`/v1/user/${id}`)
-        .set({
-          Authorization: `Bearer ${login.body.data}`,
-        });
-
-      expect(response.status).toBe(200);
-      expect(response.body.statusCode).toBe(200);
-      expect(response.body.message).toEqual('OK');
-      expect(typeof response.body.data).toBe('object');
-      expect(response.body.data).toHaveProperty('id');
-      expect(response.body.data.id).toEqual(id);
-      expect(response.body.data).toHaveProperty('createdAt');
-      expect(response.body.data).toHaveProperty('updatedAt');
-      expect(response.body.data).toHaveProperty('name');
-      expect(response.body.data).toHaveProperty('email');
-      expect(response.body.data).toHaveProperty('logo');
-    });
-
-    it('returns 401 when request is unauthorized', async () => {
-      const response = await request(app).get(`/v1/user/${id}`).set({
-        Authorization: `Bearer test`,
-      });
-
-      expect(response.status).toBe(401);
-      expect(response.body.statusCode).toBe(401);
-    });
-
-    it('returns 404 when user request is not found', async () => {
-      const response = await request(app)
-        .get(`/v1/user/7e260ebd-7a78-4584-bb97-d4bb650bc011`)
-        .set({
-          Authorization: `Bearer ${login.body.data}`,
-        });
-
-      expect(response.status).toBe(404);
-      expect(response.body.statusCode).toBe(404);
-      expect(response.body.message).toBe('The user does not exist');
-      expect(response.body.errorCode).toBe('NOT_FOUND');
-    });
-  });
-
-  describe('PUT /v1/user/{id}', () => {
-    const data = {
-      name: 'John Doe',
-      logo: 'string',
+  it('should respond with 400 Bad Request if email format is not valid', async () => {
+    const emailInvalidFormatBody = {
+      name: 'test',
+      email: 'test',
+      password: 'test',
     };
 
-    it('returns 200 when request is valid', async () => {
-      const response = await request(app)
-        .put(`/v1/user/${id}`)
-        .send(data)
-        .set({
-          Authorization: `Bearer ${login.body.data}`,
-        });
-
-      expect(response.status).toBe(200);
-      expect(response.body.statusCode).toBe(200);
-      expect(response.body.message).toEqual('OK');
-      expect(typeof response.body.data).toBe('object');
-      expect(response.body.data).toHaveProperty('id');
-      expect(response.body.data.id).toEqual(id);
-      expect(response.body.data).toHaveProperty('createdAt');
-      expect(response.body.data).toHaveProperty('updatedAt');
-      expect(response.body.data).toHaveProperty('name');
-      expect(response.body.data.name).toEqual('John Doe');
-      expect(response.body.data).toHaveProperty('email');
-      expect(response.body.data).toHaveProperty('logo');
-    });
-
-    it('returns 401 when request is unauthorized', async () => {
-      const response = await request(app).put(`/v1/user/${id}`).send(data).set({
-        Authorization: `Bearer test`,
-      });
-
-      expect(response.status).toBe(401);
-      expect(response.body.statusCode).toBe(401);
-    });
-
-    it('returns 404 when user request is not found', async () => {
-      const response = await request(app)
-        .put(`/v1/user/7e260ebd-7a78-4584-bb97-d4bb650bc011`)
-        .send(data)
-        .set({
-          Authorization: `Bearer ${login.body.data}`,
-        });
-
-      expect(response.status).toBe(404);
-      expect(response.body.statusCode).toBe(404);
-      expect(response.body.message).toBe('The user does not exist');
-      expect(response.body.errorCode).toBe('NOT_FOUND');
-    });
+    const response = await request(app).post('/v1/user/register').send(emailInvalidFormatBody);
+    expect(response.status).toBe(400);
+    expect(response.body.statusCode).toBe(400);
+    expect(response.body.message).toEqual(errorMessages.EMAIL_FORMAT_NOT_VALID);
+    expect(response.body.errorCode).toEqual(errorCodes.BAD_REQUEST);
   });
+
+  it('should respond with 409 Conflict if email already exists', async () => {
+    await request(app).post('/v1/user/register').send(completeBody);
+    const response = await request(app).post('/v1/user/register').send(completeBody);
+
+    expect(response.status).toBe(409);
+    expect(response.body.statusCode).toBe(409);
+    expect(response.body.message).toEqual(errorMessages.EMAIL_ALREADY_EXISTS);
+    expect(response.body.errorCode).toEqual(errorCodes.CONFLICT);
+  });
+});
+
+afterEach(async () => {
+  app.close();
+  await prisma.user.deleteMany();
+  await prisma.$disconnect();
 });
